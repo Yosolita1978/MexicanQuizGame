be59124c433834dc076270e0ac3bf387211eb2a8
Cristina Rodriguez <cristina@Cristinas-MacBook-Pro.local>
2021-03-18 17:07:36 -0700

    first bunch of data

---
+++ Data/questions.js
@@ -0,0 +1,126 @@
+const totalQuestions = [{
+    id: 1,
+    title: "Deidad del México antiguo conocida como la “Serpiente Emplumada”",
+    choices: ["Coatlicue", "Huitzilopochtli", "Tláloc", "Quetzalcóatl"],
+    correct: "Quetzalcóatl"
+},
+{
+    id: 2,
+    title: "Cultura que se desarrolló en la ciudad de Palenque:",
+    choices: ["Maya", "Mexica", "Olmeca", "Tolteca"],
+    correct: "Maya",
+},
+{
+    id: 3,
+    title: "Las cabezas colosales son creaciones de la cultura:",
+    choices: ["Mexica", "Mixteca", "Olmeca", "Tolteca"],
+    correct: "Olmeca"
+},
+{
+    id: 4,
+    title: "Terreno  construido  sobre  las  lagunas  del  valle  de  México  que  sirvió  de  base  al  sistema  productivo  del altiplano central.",
+    choices: ["Chinampa", "Encomienda", "Ejido", "Hacienda"],
+    correct: "Chinampa"
+},
+{
+    id: 5,
+    title: "Manuscrito en papel amate o sobre cuero con representaciones pictóricas que relataba asuntos históricos y religiosos del México antiguo:",
+    choices: ["Jeroglífico", "Pictograma", "Libro", "Códice"],
+    correct: "Códice"
+},
+{
+    id: 6,
+    title: "¿En dónde se encuentran las pirámides del Sol y la Luna?",
+    choices: ["Palenque ", "Chichén Itzá", "Teotihuacan", "Uxmal"],
+    correct: "Teotihuacan"
+},
+{
+    id: 7,
+    title: "Al lugar mítico de donde partieron los mexicas e iniciaron su migración se le conoce como:",
+    choices: ["Aztlán", "Tula", "Mictlán", "Tenochtitlán "],
+    correct: "Aztlán",
+},
+{
+    id: 8,
+    title: "La base alimenticia de los pueblos mesoamericanos fue:",
+    choices: ["La papa", "El trigo", "El arroz", "El maíz"],
+    correct: "El maíz"
+},
+{
+    id: 9,
+    title: "Chichén Itzá fue una poderosa ciudad:",
+    choices: ["Tolteca", "Maya", "Totonaca", "Olmeca"],
+    correct: "Maya"
+},
+{
+    id: 10,
+    title: "Año en que los españoles conquistaron México-Tenochtitlán:",
+    choices: ["1521", "1555", "1650", "1689"],
+    correct: "1521"
+},
+{
+    id: 11,
+    title: "Nombre de México durante el periodo colonial:",
+    choices: ["México", "Nueva España", "Tenochtitlán", "Nueva Granada"],
+    correct: "Nueva España"
+},
+{
+    id: 12,
+    title: "Nombre del conquistador que derrotó al Imperio mexica:",
+    choices: ["Hernán Cortés", "Francisco Pizarro", "Pánfilo de Narváez", "Cristóbal Colón"],
+    correct: "Hernán Cortés",
+},
+{
+    id: 13,
+    title: "Nombre del último emperador mexica:",
+    choices: ["Moctezuma", "Cuauhtémoc", "Cuitláhuac", "Tizoc"],
+    correct: "Cuauhtémoc"
+},
+{
+    id: 14,
+    title: "'La Malinche' fue:",
+    choices: ["La intérprete de Cortés", "Una heroína", "Una famosa curandera", "La primera diputada"],
+    correct: "La intérprete de Cortés"
+},
+{
+    id: 15,
+    title: "Nombre  de  la  batalla  en  la  que  se  afirma  que  los  españoles  perdieron  hombres,  caballos  y cañones  en 1520",
+    choices: ["De la Calzada de Tacuba", "Derrota de Atizapán", "De la Noche Triste", "Matanza de Cholula"],
+    correct: "De la Noche Triste"
+},
+{
+    id: 16,
+    title: " La máxima autoridad civil en la Nueva España era el:",
+    choices: ["Virrey", "Gobernador", "Presidente", "Primer Ministro"],
+    correct: "Virrey"
+},
+{
+    id: 17,
+    title: "La 'encomienda' y el 'visitador' fueron instituciones:",
+    choices: ["Prehispánicas", "Coloniales", "Del México recién independizado", "Del gobierno de Lázaro Cárdenas"],
+    correct: "Coloniales",
+},
+{
+    id: 18,
+    title: "En México, a partir del virreinato, se denominaba criollos a:",
+    choices: ["Hijos de padre español y madre indígena", "Hijos de esclavos negros", "Todas las personas que no tuvieran tez blanca", "Hijos de españoles nacidos en la Nueva España"],
+    correct: "Hijos de españoles nacidos en la Nueva España"
+},
+{
+    id: 19,
+    title: "Acapulco fue durante los siglos XVII y XVIII un importante puerto porque controlaba la navegación en el:",
+    choices: ["Océano Pacífico", "Golfo de México", "Océano Atlántico", "Mar Caribe"],
+    correct: "Océano Pacífico"
+},
+{
+    id: 20,
+    title: "El principal culto religioso de la sociedad novohispana fue:",
+    choices: ["Virgen de los Remedios", "Virgen de Guadalupe", "Cristo Negro de Chalma", "San Judas Tadeo"],
+    correct: "Virgen de Guadalupe"
+}
+
+
+]
+
+
+export default totalQuestions;

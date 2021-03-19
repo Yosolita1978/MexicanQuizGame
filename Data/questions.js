b2d187a8d22dc5c72459d64b23b8d69aebd22383
Cristina Rodriguez <cristina@Cristinas-MacBook-Pro.local>
2021-03-18 21:42:59 -0700

    More questions in data

--- Data/questions.js
+++ Data/questions.js
@@ -117,6 +117,186 @@ const totalQuestions = [{
     title: "El principal culto religioso de la sociedad novohispana fue:",
     choices: ["Virgen de los Remedios", "Virgen de Guadalupe", "Cristo Negro de Chalma", "San Judas Tadeo"],
     correct: "Virgen de Guadalupe"
+},
+{
+    id: 21,
+    title: "El mineral que más se extraía en la Nueva Españaera:",
+    choices: ["Níquel", "Cobre", "Estaño", "Plata"],
+    correct: "Plata"
+},
+{
+    id: 22,
+    title: "Nombre de la poetisa más importante de la época novohispana:",
+    choices: ["Sor Juana Inés de la Cruz", "María Anna Agueda de San Ignacio", "La Malinche", "Tonantzin"],
+    correct: "Sor Juana Inés de la Cruz",
+},
+{
+    id: 23,
+    title: "Fecha en la que se celebra la Independencia de México:",
+    choices: ["5 de mayo", "5 de febrero ", "16 de septiembre", "20 de noviembre"],
+    correct: "16 de septiembre"
+},
+{
+    id: 24,
+    title: "La Independencia de México inició en: ",
+    choices: ["1810", "1821", "1910", "1921"],
+    correct: "1810"
+},
+{
+    id: 25,
+    title: "¿Cuál fue la importancia del sacerdote Miguel Hidalgo y Costilla en la historia de México?",
+    choices: ["Inició la Guerra de Independencia", "Inició la Revolución mexicana", "Promulgó una nueva constitución", "Encabezó la Guerra de Reforma"],
+    correct: "Inició la Guerra de Independencia"
+},
+{
+    id: 26,
+    title: "¿A quién se conoce como “El Padre de la Patria”?",
+    choices: ["Francisco I. Madero", "Benito Juárez", "Miguel Hidalgo y Costilla", "George Washington"],
+    correct: "Miguel Hidalgo y Costilla"
+},
+{
+    id: 27,
+    title: "Cadetes militares que defendieron el Castillo de Chapultepec de la invasión norteamericana en 1847:",
+    choices: ["Insurgentes ", "Niños héroes", "Defensores de la Patria", "Liberales"],
+    correct: "Niños héroes",
+},
+{
+    id: 28,
+    title: "En México el 5 de mayo se celebra:",
+    choices: ["La Batalla de Puebla", "La Revolución mexicana", "La promulgación de la Constitución", "La Independencia de México"],
+    correct: "La Batalla de Puebla"
+},
+{
+    id: 29,
+    title: "A quién se le atribuye la frase 'Entre los individuos como entre las naciones, el respeto al derecho ajeno es la paz'",
+    choices: ["Benito Juárez", "Francisco I. Madero", "Emiliano Zapata", "Lázaro Cárdenas"],
+    correct: "Benito Juárez"
+},
+{
+    id: 30,
+    title: "México perdió la mitad de su territorio a mediados del siglo XIX, en un enfrentamiento con:",
+    choices: ["Francia", "España", "Portugal", "Estados Unidos"],
+    correct: "Estados Unidos"
+},
+{
+    id: 31,
+    title: "La Guerra de Reforma fue entre:",
+    choices: ["México y Estados Unidos", "Liberales y conservadores", "Ejército mexicano y Ejército francés", "México y Francia"],
+    correct: "Liberales y conservadores"
+},
+{
+    id: 32,
+    title: "El presidente que duró más de treinta años en el poder en México fue:",
+    choices: ["Antonio López de Santa Anna", "Porfirio Díaz", "Benito Juárez", "Francisco I. Madero"],
+    correct: "Porfirio Díaz",
+},
+{
+    id: 33,
+    title: "El Porfiriato se conoce como una época de extensión de:",
+    choices: ["Las vías férreas", "La esclavitud", "El ejido", "La radio"],
+    correct: "Las vías férreas"
+},
+{
+    id: 34,
+    title: "Fecha en que se celebra el aniversario de la Revolución mexicana:",
+    choices: ["5 de febrero", "5 de mayo", "16 de septiembre", "20 de noviembre"],
+    correct: "20 de noviembre"
+},
+{
+    id: 35,
+    title: "Nombre del líder agrario que encabezó la Revolución mexicana en el estado de Morelos:",
+    choices: ["Venustiano Carranza", "Emiliano Zapata", "Francisco I. Madero", "Francisco Villa"],
+    correct: "Emiliano Zapata"
+},
+{
+    id: 36,
+    title: "El lema “Sufragio efectivo, no reelección” fue la bandera política de:",
+    choices: ["Francisco I. Madero", "Benito Juárez", "Plutarco E. Calles", "Francisco Villa"],
+    correct: "Francisco I. Madero"
+},
+{
+    id: 37,
+    title: "¿Quién fue Francisco Villa?",
+    choices: ["Un famoso bandido del siglo XIX", "Un líder de la Revolución mexicana", "Un personaje de la novela 'Sálvese quien pueda'", "Un cantante de música vernácula"],
+    correct: "Un líder de la Revolución mexicana",
+},
+{
+    id: 38,
+    title: " Las 'Adelitas' acompañaron a sus hombres al combate en:",
+    choices: ["La Guerra de los Pasteles", "La Guerra de Independencia", "La Revolución de 1910", "La Guerra de 1847"],
+    correct: "La Revolución de 1910"
+},
+{
+    id: 39,
+    title: "Composición musical característica de la época revolucionaria:",
+    choices: ["El corrido", "El vals", "El jarabe tapatío", "El mariachi"],
+    correct: "El corrido"
+},
+{
+    id: 40,
+    title: "El 18 de marzo de 1938 el presidente Lázaro Cárdenas:",
+    choices: ["Expropió el petróleo", "Redactó las Leyes de Reforma", "Nacionalizó la banca", "Inició la Revolución mexicana"],
+    correct: "Expropió el petróleo"
+},
+{
+    id: 41,
+    title: "Señale el orden cronológico correcto de los siguientes acontecimientos históricos:",
+    choices: ["Revolución mexicana, Independencia, Conquista", "Conquista, Revolución mexicana, Independencia", "Independencia, Conquista, Revolución mexicana", "Conquista, Independencia, Revolución mexicana"],
+    correct: "Conquista, Independencia, Revolución mexicana"
+},
+{
+    id: 42,
+    title: "Representante del muralismo mexicano:",
+    choices: ["Diego Rivera", "Octavio Paz", "Luis Barragán", "Agustín Lara"],
+    correct: "Diego Rivera",
+},
+{
+    id: 43,
+    title: "Nombre del partido político que gobernó durante siete décadas en el siglo XX:",
+    choices: ["Partido Comunista", "Partido Socialista Único de México", "Partido Revolucionario Institucional", "Partido Auténtico de la Revolución Mexicana"],
+    correct: "Partido Revolucionario Institucional"
+},
+{
+    id: 44,
+    title: "¿En qué conflicto bélico internacional participó el “Escuadrón 201”?",
+    choices: ["Guerra de Texas", "Guerra de los Pasteles", "Primera Guerra Mundial", "Segunda Guerra Mundial"],
+    correct: "Segunda Guerra Mundial"
+},
+{
+    id: 45,
+    title: "El Tratado de Tlatelolco, logro de la diplomacia mexicana, atañe a:",
+    choices: ["Los derechos sobre el mar patrimonial", "Los derechos de los pueblos indígenas", "La no proliferación de las armas nucleares", "Los intercambios comerciales en América Latina"],
+    correct: "La no proliferación de las armas nucleares"
+},
+{
+    id: 46,
+    title: "Los símbolos patrios de México son:",
+    choices: ["El Escudo, la Bandera y el Himno Nacional", "La Bandera, la Moneda y el Himno Nacional", "El Águila Real, el Escudo y el Palacio Nacional", "El Ángel de la Independencia, el Himno Nacional y la Bandera"],
+    correct: "El Escudo, la Bandera y el Himno Nacional"
+},
+{
+    id: 47,
+    title: " El tema dominante del himno nacional mexicano es:",
+    choices: ["La unidad latinoamericana", "La guerra defensiva", "El mestizaje en México ", "La paz internacional"],
+    correct: "La guerra defensiva",
+},
+{
+    id: 48,
+    title: "En el himno nacional, la expresión 'Mas si osare un extraño enemigo' significa:",
+    choices: ["Son peores que osos nuestros enemigos", "Abundan los huesos de nuestros enemigos", "Pero si se atreviera un enemigo extranjero", "Con más razón si el enemigo fuera extravagante"],
+    correct: "Pero si se atreviera un enemigo extranjero"
+},
+{
+    id: 49,
+    title: "El águila y la serpiente que aparecen en el escudo nacional mexicano:",
+    choices: ["Figuraban en el escudo de armas de un conquistador", "Son una respuesta nacionalista al águila estadounidense", "Evocan una leyenda prehispánica", "Aparecían en el estandarte de Miguel Hidalgo"],
+    correct: "Evocan una leyenda prehispánica"
+},
+{
+    id: 50,
+    title: "¿Cuántos estados tiene la República Mexicana?",
+    choices: ["22", "31", "36", "50"],
+    correct: "31"
 }
 
 
